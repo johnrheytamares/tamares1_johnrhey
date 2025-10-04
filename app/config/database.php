f7e8b4524a2acd7a95052a9586ae2cca7c43407d
johnrhey <johnrheynedamotamares@gmail.com>
2025-10-04 17:39:40 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -63,7 +63,7 @@ $database['main'] = array(
     'port'      => '3306',
     'username'	=> 'sql12801271',
     'password'	=> 'MYUFJpiY7g',
-    'database'	=> 'ql12801271',
+    'database'	=> 'sql12801271',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
